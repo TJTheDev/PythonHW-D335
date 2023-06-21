7c695003141166b217950131cf7e281cf3edcf82
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-06-21 16:05:05 -0500

    Update and rename PreDefList.py to 7GreaterThanList.py

--- 7GreaterThanList.py
+++ 7GreaterThanList.py
@@ -16,13 +16,12 @@ If the input is
 then the expected output is
 
 Greater Than Max? False
-"""
 
+OLD:
 predef_list = [4, -27, 15, 33, -10]
 
 #solution accepts an integer input
 userInput = int(input())
-
 
 #solution outputs Boolean value indicating if integer input is greater than the maximum value when compared to predef_list
 compareMax = max(predef_list)
@@ -32,3 +31,16 @@ else:
     boolX = False
     
 print ("Greater Than Max?", boolX)
+"""
+
+# Define the predefined list of integers
+predef_list = [4, -27, 15, 33, -10]
+
+# Get input for the user's integer value
+userInput = int(input())  # Enter an integer value for comparison
+
+# Compare the user's input with the maximum value from the predefined list
+compareMax = userInput > max(predef_list)  # Check if userInput is greater than the maximum value in predef_list
+
+# Print the result indicating if the user's input is greater than the maximum value
+print("Greater Than Max?", compareMax)  # Print a Boolean value indicating if userInput is greater than the maximum value
