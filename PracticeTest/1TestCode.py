2e4b228bd227851fc8a97aea007031b35089a727
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-06-23 10:41:57 -0500

    Update 1TestCode.py

--- PracticeTest/1TestCode.py
+++ PracticeTest/1TestCode.py
@@ -12,6 +12,23 @@ plural_noun = str(input())
 
 # TODO: Type your code to read three more values here.
 
-
 # Output a short story using the four input values. Do not modify the code below.
 print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
+
+# Convert to dollars
+''' Given four values representing counts of quarters, dimes, nickels and pennies, 
+output the total amount as dollars and cents. 
+
+Output each floating-point value with two digits after the decimal point, which can be achieved as follows:
+print(f'Amount: ${dollars:.2f}')
+'''
+
+quarterTotal = int(input()) * (.25)
+dimesTotal = int(input()) * (.10)
+nickelsTotal = int(input()) * (.05)
+penniesTotal = int(input()) * (.01)
+totalCount = (quarterTotal + dimesTotal + nickelsTotal + penniesTotal)
+
+print(f"Amount: ${totalCount:.2f}")
+
+
