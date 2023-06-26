15c084c1fcd8541a8693e4bcddcfe418f44a38a5
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-06-26 15:16:40 -0500

    Update 2ch33_11-.py

--- PracticeTest/2ch33_11-.py
+++ PracticeTest/2ch33_11-.py
@@ -236,3 +236,61 @@ while loop is True:
         
         # Output the date in the required format using string formatting
         print(f"{get_month_as_int(dateSplit2nd[0])}/{dateSplit2nd[1]}/{dateSplitOG[1]}")
+"""
+Write a program to calculate the total price for car wash services. A base car wash is $10. A dictionary with each additional service and the corresponding cost has been provided. Two additional services can be selected. A '-' signifies an additional service was not selected. Output all selected services, according to the input order, along with the corresponding costs and then the total price for all car wash services.
+
+Ex: If the input is:
+
+Tire shine
+Wax
+the output is:
+
+ZyCar Wash
+Base car wash -- $10
+Tire shine -- $2
+Wax -- $3
+----
+Total price: $15
+Ex: If the input is:
+
+Rain repellent
+-
+the output is:
+
+ZyCar Wash
+Base car wash -- $10
+Rain repellent -- $2
+----
+Total price: $12
+"""
+
+services = { 'Air freshener': 1, 'Rain repellent': 2, 'Tire shine': 2, 'Wax': 3, 'Vacuum': 5 }
+base_wash = 10
+total = 0
+
+# Get user input for service choices
+service_choice1 = input()
+service_choice2 = input()
+
+# Print the car wash header
+print("ZyCar Wash")
+print("Base car wash -- $10")
+total = base_wash
+
+# Check if the first service choice is selected
+if service_choice1 != "-":
+    # Print the first service choice and its price
+    print(f"{service_choice1} -- ${services[service_choice1]}")
+    total = total + services[service_choice1]
+
+# Check if the second service choice is selected
+if service_choice2 != "-":
+    # Print the second service choice and its price
+    print(f"{service_choice2} -- ${services[service_choice2]}")
+    total = total + services[service_choice2]
+
+# Print a separator
+print("----")
+
+# Print the total price
+print(f"Total price: $" + str(total))
