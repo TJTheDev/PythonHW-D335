e1cfb306a32a75407a3f278fc5f5712a8974db69
Tarrylon Toney <90969441+TJTheDev@users.noreply.github.com>
2023-07-06 13:52:38 -0500

    Update 02OuncesConverter.py

--- Ch34/02OuncesConverter.py
+++ Ch34/02OuncesConverter.py
@@ -10,18 +10,18 @@ Ounces: value_3
 
 """
 
-tonsOut = 0
-poundsOut = 0
-ounces = int(input())
+tonsOut = 0  # Variable to store the number of tons
+poundsOut = 0  # Variable to store the number of pounds
+ounces = int(input())  # Get input value in ounces
 
-while ounces >= (2000 * 16):
-    tonsOut = tonsOut + 1
-    ounces = ounces - (2000 * 16)
+while ounces >= (2000 * 16):  # Check if the input value is greater than or equal to one ton (2000 pounds)
+    tonsOut = tonsOut + 1  # Increment the tons count
+    ounces = ounces - (2000 * 16)  # Subtract one ton (2000 pounds) from the input value
 
-while ounces >= 16:
-    poundsOut = poundsOut + 1
-    ounces = ounces - 16
+while ounces >= 16:  # Check if the remaining ounces are greater than or equal to one pound
+    poundsOut = poundsOut + 1  # Increment the pounds count
+    ounces = ounces - 16  # Subtract one pound from the remaining ounces
 
-print (f"Tons: {tonsOut}")
-print (f"Pounds: {poundsOut}")
-print (f"Ounces: {ounces}")
+print(f"Tons: {tonsOut}")  # Print the number of tons
+print(f"Pounds: {poundsOut}")  # Print the number of pounds
+print(f"Ounces: {ounces}")  # Print the remaining ounces
